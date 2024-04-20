11d6f4ab86e8da6e5c578bfa999224f3ce37ad59
EldorMouyal <93645504+EldorMouyal@users.noreply.github.com>
2024-04-20 10:16:43 +0300

    added: filter_sharp_anomalies filter_obtuse_anomalies some helper function added as well K means method modified with anti line intersection mechanism

--- cube_solver.py
+++ cube_solver.py
@@ -32,6 +32,7 @@ import bisect
 #              |************|
 #              |*D7**D8**D9*|
 #              |************|
+# TODO: define constant variables such as frame size, vertical angle, sharp andle, dull angle...
 
 
 def display_image(image, title):
@@ -54,6 +55,39 @@ def polar_to_cartesian(rho, theta):
     return x, y
 
 
+def find_intersection_point_two_lines(line1, line2, frame_width, frame_height):
+    rho1, theta1 = line1
+    rho2, theta2 = line2
+    # Convert polar lines to Cartesian coordinates
+    x1, y1 = polar_to_cartesian(rho1, theta1)
+    x2, y2 = polar_to_cartesian(rho2, theta2)
+    # Find intersection point in Cartesian coordinates
+    if np.sin(theta1 - theta2) == 0:
+        return -1, -1  # Lines are parallel
+    x_intersect = (rho1 * np.cos(theta2) - rho2 * np.cos(theta1)) / np.sin(theta1 - theta2)
+    y_intersect = (rho1 * np.sin(theta2) - rho2 * np.sin(theta1)) / np.sin(theta2 - theta1)
+    # Check if intersection point is within the frame size
+    if 0 <= x_intersect <= frame_width and 0 <= y_intersect <= frame_height:
+        return x_intersect, y_intersect
+    else:
+        return -1, -1
+
+
+def find_intersection_point_neg_slope(line_rho, line_theta, im_size):
+    # Convert polar line to Cartesian coordinates
+    line_x, line_y = polar_to_cartesian(line_rho, line_theta)
+
+    # Find intersection point with the line y = im_size - x
+    x_intersect = (im_size - line_y + line_x) / 2
+    y_intersect = im_size - x_intersect
+
+    # Check if intersection point lies within the bounds of the frame
+    if 0 <= x_intersect <= im_size and 0 <= y_intersect <= im_size:
+        return x_intersect, y_intersect
+    else:
+        return None
+
+
 def extract_pairs(array_of_arrays):
     res = []
     for sub_array in array_of_arrays:
@@ -73,19 +107,53 @@ def find_x_given_y(rho, theta, y):
         return x
 
 
-def filter_vertical_anomalies(lines, y_value=200, Y=400):
+def filter_vertical_anomalies(rho_theta, im_size):
     n_bins = 10
     # Divide lines into Bins
+    y_value = im_size/2
     bins = [[] for _ in range(n_bins)]
-    for rho, theta in lines:
+    for rho, theta in rho_theta:
         x_val = int(find_x_given_y(rho, theta, y_value))
-        index = x_val / (Y/n_bins)
-        print(index)
+        index = x_val / (im_size / n_bins)
+        # print(index)
         if 0 <= index < n_bins:
             bins[int(index)].append([rho, theta])
     print(bins)
     # filtered = filter_anomalies_theta(bins, 0.2)
-    filtered = filter_closest_pair_to_average(bins)
+    filtered = filter_closest_pair_to_average_theta(bins)
+    return extract_pairs(filtered)
+
+
+def filter_sharp_anomalies(rho_theta, im_size):
+    n_bins = 30
+    bins = [[] for _ in range(n_bins)]
+    c_length = int(np.sqrt(2 * (im_size**2)))
+    for rho, theta in rho_theta:
+        x, y = polar_to_cartesian(rho, theta)
+        dist_from_TL = int(np.sqrt(x**2 + x**2))  # TL = Top Left
+        index = dist_from_TL / (c_length / n_bins)
+        if 0 <= index < n_bins:
+            bins[int(index)].append([rho, theta])
+    print(bins)
+    filtered = filter_closest_pair_to_average_rho(bins)
+    return extract_pairs(filtered)
+
+
+def filter_obtuse_anomalies(rho_theta, im_size):
+    n_bins = 30
+    bins = [[] for _ in range(n_bins)]
+    c_length = int(np.sqrt(2 * (im_size**2)))
+    for rho, theta in rho_theta:
+        x, y = polar_to_cartesian(rho, theta)
+        x_intersect = (im_size - y + x)/2
+        y_intersect = im_size - x_intersect
+        if 0 <= x_intersect <= im_size and 0 <= y_intersect <= im_size:
+            dist_from_TR = int(np.sqrt((x_intersect - im_size)**2 + y_intersect**2))
+            index = dist_from_TR / (c_length / n_bins)
+            if 0 <= index < n_bins:
+                bins[int(index)].append([rho, theta])
+    print(bins)
+    filtered = filter_closest_pair_to_average_rho(bins)
     return extract_pairs(filtered)
 
 
@@ -106,7 +174,7 @@ def filter_anomalies_theta(bins, threshold_percent):
     return filtered_bins
 
 
-def filter_closest_pair_to_average(bins):
+def filter_closest_pair_to_average_theta(bins):
     filtered_bins = []
     for _bin in bins:
         if not _bin:  # Skip empty bins
@@ -120,49 +188,37 @@ def filter_closest_pair_to_average(bins):
     return filtered_bins
 
 
+def filter_closest_pair_to_average_rho(bins):
+    filtered_bins = []
+    for _bin in bins:
+        if not _bin:  # Skip empty bins
+            continue
+        # Calculate the average of the second values
+        first_values = [pair[0] for pair in _bin]
+        avg_rho = sum(first_values) / len(first_values)
+        # Find the pair whose second value is closest to the average
+        closest_pair = min(_bin, key=lambda pair: abs(pair[1] - avg_rho))
+        filtered_bins.append([closest_pair])
+    return filtered_bins
+
+
 def k_means_for_lines(lines):
-    rhos = []
-    avg_thetas = 0
-    for line in lines:
-        rho, theta = line[0]
-        is_distinct = True
-        # Check if rho is distinct from every other rho by more than 3
-        for existing_rho in rhos:
-            if abs(rho - existing_rho) <= 20:
-                is_distinct = False
-                break
-        if is_distinct:
-            rhos.append(rho)
-            avg_thetas += theta
-    avg_thetas = avg_thetas/(len(rhos))
-    print(rhos)
-    print(avg_thetas)
-    rho_2d = np.array(rhos).reshape(-1, 1)
-    kmeans = KMeans(n_clusters=7)
-    kmeans.fit(rho_2d)
-    # cluster_centers = sorted(kmeans.cluster_centers_)
-    print(kmeans.cluster_centers_)
-    return kmeans.cluster_centers_, avg_thetas
-
-
-def k_means_for_lines2(lines):
     rho_theta = []
     rhos = []
-    avg_thetas = 0
     for rho, theta in lines:
-        is_distinct = True
-        # Check if rho is distinct from every other rho by more than 3
-        for existing_rho in rhos:
-            if abs(rho - existing_rho) <= 20:
-                is_distinct = False
+        is_intersect = False
+        # Check that if a line is chosen it does not intersect inside the picture
+        for _rho, _theta in rho_theta:
+            x, y = find_intersection_point_two_lines([rho, theta], [_rho, _theta], 400, 400)
+            if x != -1 and y != -1:
+                is_intersect = True
                 break
-        if is_distinct:
+        if not is_intersect:
             rho_theta.append([rho, theta])
             rhos.append(rho)
-            avg_thetas += theta
-    avg_thetas = avg_thetas / (len(rhos))
     print(rho_theta)
-    print(avg_thetas)
+    if len(rho_theta) < 7:
+        print("Picture not good enough")
     rho_2d = np.array(rhos).reshape(-1, 1)
     kmeans = KMeans(n_clusters=7)
     kmeans.fit(rho_2d)
@@ -242,12 +298,17 @@ def find_grid_for_theta(image, theta: int):
         headline = "Obtuse theta"
     edges = display_canny(image)
     lines = hough_lines_for_theta(image=image, edges=edges, theta=theta, headline=headline)
-    lines = filter_vertical_anomalies(lines[:, 0], Y=image.shape[1], y_value=image.shape[1]/2)
+    if theta == 0:
+        lines = filter_vertical_anomalies(lines[:, 0], im_size=image.shape[1])
+    elif theta == 60:
+        lines = filter_sharp_anomalies(lines[:, 0], image.shape[1])
+    elif theta == 110:
+        lines = filter_obtuse_anomalies(lines[:, 0], image.shape[0])
     draw_lines_by_polar(image=image, rho_theta=lines, color=(0, 255, 0))
     display_image(image, "after filtering")
     # rhos, avg_theta = k_means_for_lines(lines)
     # rhos, avg_theta = distinct_take_lines(lines)
-    rho_theta = k_means_for_lines2(lines)
+    rho_theta = k_means_for_lines(lines)
     draw_lines_by_polar(image=image, rho_theta=rho_theta, color=(255, 0, 0), thickness=2)
     display_image(image, headline)
     return
@@ -294,7 +355,7 @@ def main(name):
     # lines = hough_lines_for_theta(image=image, edges=edges, theta=60, headline="Sharp Angle Lines")
     # lines = hough_lines_for_theta(image=image, edges=edges, theta=120, headline="Obtuse Angle Lines")
     # lines = hough_lines_for_theta(image=image, edges=edges, theta=0, headline="Vertical Lines")
-    find_grid_for_theta(resized_image, theta=0)
+    find_grid_for_theta(resized_image, theta=110)
     return
 
 
